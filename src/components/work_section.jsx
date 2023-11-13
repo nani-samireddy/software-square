f9e7438c504c43ca6e6e0a2050f878bfcea3901a
nani-samireddy <nanisamireddy05@gmail.com>
2023-11-13 17:13:12 +0530

    updated the work card

--- src/components/work_section.jsx
+++ src/components/work_section.jsx
@@ -8,19 +8,19 @@ import MotionH1 from "./motion_h1";
 export default function WorkSection() {
   return (
     <div>
-      <MotionP styles={"py-3 w-max"}>Recent works</MotionP>
+      <MotionP styles={"py-3 w-max"}>[Recent works]</MotionP>
       <Divider />
       {workData.map((work) => (
         <>
           <div
             key={work.key}
-            className="py-8 md:py-2 flex justify-between items-center"
+            className="py-8 md:py-0 flex justify-between items-center"
           >
             <MotionH1 styles="font-bold text-xl md:text-2xl lg:text-5xl pl-5">
               {work.title}
             </MotionH1>
             <motion.img
-              className="hidden md:block w-auto h-36 object-cover "
+              className="hidden md:block w-auto h-40 object-cover "
               src={work.images[0]}
             />
           </div>
