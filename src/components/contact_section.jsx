454a7030f68e40896634a39f5498830d50755d29
nani-samireddy <nanisamireddy05@gmail.com>
2023-11-13 21:39:49 +0530

    added contact

--- src/components/contact_section.jsx
+++ src/components/contact_section.jsx
@@ -1,13 +1,43 @@
-import { Divider } from "@nextui-org/react";
+import { Button, Divider, Input, Textarea } from "@nextui-org/react";
 import React from "react";
+import { useMouseContext } from "../contexts/mouse_context";
 
 export default function ContactSection() {
+  const { mouseTextEnter, mouseTextLeave } = useMouseContext();
   return (
     <div>
-      <h1 className="text-6xl text-gray-300 font-black py-2">
+      <h1 className="text-6xl text-center text-gray-300 font-black py-2">
         Connect With Us
       </h1>
-      <Divider />
+      <form
+        action=""
+        method="post"
+        className=" flex flex-col justify-center items-center h-auto"
+      >
+        <div className="w-[40vw] py-10 flex flex-col gap-6">
+          <Input
+            type="name"
+            label="Name"
+            variant="underlined"
+            placeholder="Enter your name"
+          />
+
+          <Input
+            type="email"
+            label="Email"
+            variant="underlined"
+            placeholder="Enter your Email"
+          />
+          <Textarea placeholder="Your idea here..." variant="underlined" />
+          <button
+            onMouseEnter={mouseTextEnter}
+            onMouseLeave={mouseTextLeave}
+            className="font-light text-xl rounded-md py-3 bg-black text-white"
+          >
+            SEND MESSAGE
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
